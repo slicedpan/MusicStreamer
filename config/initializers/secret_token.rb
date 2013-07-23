56317cda6f0de64d72ed54fd876c491f984630ec
Owen Mooney <owen.m@bccriskadvisory.com>
2013-07-23 18:20:23 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicStreamer::Application.config.secret_key_base = '2a6c5a25f8261ba166c0163423a08093aa7b930f600e011e379c9fdb1e8bb7b9095a8432975043e503b6caeb371cd6e0e05cfad5fd32715f3a1609bddea3047f'
